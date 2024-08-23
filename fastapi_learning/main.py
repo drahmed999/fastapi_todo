56c42b2f1f572f3f62ded04aa714f66f765d9989
drahmed999 <ahmedqureshi999@gmail.com>
2024-08-23 17:16:48 +0500

    change

--- fastapi_learning/main.py
+++ fastapi_learning/main.py
@@ -45,7 +45,7 @@ async def lifespan(app:FastAPI):
 
 app=FastAPI(lifespan=lifespan,
             title="hello world db",
-            # version="0.0.1",
+            version="0.0.1",
             servers=[{
                 "url":"http://127.0.0.1:8000",
                 "description":"fast_api"
